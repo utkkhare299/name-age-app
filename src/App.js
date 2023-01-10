f21ab34510bbfe20bc6ba18486a0639506e2b1ba
utkarsh khare <utkkhare999@gmail.com>
2023-01-10 13:41:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import AddUser from "./components/User/AddUser";
+import UserList from "./components/User/UserList";
+
 
 function App() {
+  const [usersList, setUsersList]= useState([]);
+  const addUserHandler = (uName,uAge) => {
+    setUsersList((prevUsersList)=>{
+      return [...prevUsersList ,{name:uName, age:uAge,id:Math.random().toString()}];
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users= {usersList} />
+      </React.Fragment>
   );
 }
 
